32d266b1c2228dd91c52d9524600d97161d0808e
LucasJ27 <lucasjeremiasc@gmail.com>
2023-08-22 20:36:46 -0300

    Arreglo punto 20

--- TP1Parte2.py
+++ TP1Parte2.py
@@ -211,8 +211,11 @@ año = int(input("Ingresa el año de tu nacimiento: "))
 print(f"La fecha de nacimiento del usuario es: {dia}/{mes}/{año}")
 
 #20 Hacer otra versión del programa, pero esta vez almacenado todo en una única variable con formato DDMMAAA.
-año_nacimiento = f"{dia}/{mes}/{año}"          
-print(f"La fecha de nacimiento del usuario es: {año_nacimiento}")
+fecha_nacimiento = input("Ingresa tu fecha de nacimiento en formato DDMMAAA: ")
+dia = fecha_nacimiento[0:2]
+mes = fecha_nacimiento[2:4]
+año = fecha_nacimiento[4:8]
+print(f"La fecha de nacimiento del usuario es: {dia}/{mes}/{año}")
 
 #21 Una pareja de motociclistas necesita hacer ciertos cálculos antes de emprender un viaje en moto, para saber cuántos tanques de combustible consumirá el viaje entero.
 # Para eso deben ingresar: cuántos kilómetros puede recorrer su moto con 1 litro de combustible, qué capacidad (en litros) tiene el tanque y cuántos kilómetros en total recorrerán.
